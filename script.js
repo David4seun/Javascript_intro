385952a35be4a9fcbe8e3ac5f2ebade597d7d400
David4seun <105640781+David4seun@users.noreply.github.com>
2022-06-23 13:34:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ let myname = "Oluwaseun Ogundipe";
 let country = "Nigeria";
 let height = "5'11"
 
-window.alert("My name is" + myname + " and I'm " + height + "tall and I'm from " + country)
+window.alert("My name is " + myname + " and I'm " + height + "tall and I'm from " + country)
